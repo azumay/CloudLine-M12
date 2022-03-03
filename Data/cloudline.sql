d11790e3db736e01b4e6d2ff21846be2d93dba18
Xavi Yamuza <xavi_ym@hotmail.com>
2022-03-03 19:20:08 +0100

    creacion-tablas

--- Data/cloudline.sql
+++ Data/cloudline.sql
@@ -18,7 +18,7 @@ USE cloudline;
 DROP TABLE IF EXISTS 'cliente';
 
 CREATE TABLE IF NOT EXISTS cliente (
-  id int(11) NOT NULL AUTO_INCREMENT,
+  id int NOT NULL AUTO_INCREMENT,
   nombre varchar(75) NOT NULL,
   apellidos varchar(155)  NOT NULL,
   telefono varchar(9) NOT NULL,
@@ -37,8 +37,8 @@ CREATE TABLE IF NOT EXISTS cliente (
 
 DROP TABLE IF EXISTS 'empleado';
 
-CREATE TABLE IF NOT EXISTS cola (
-  id int(11) NOT NULL ,
+CREATE TABLE IF NOT EXISTS empleado (
+  id int NOT NULL ,
   id_centro int(11) NOT NULL ,
   nombre varchar(75)  NOT NULL,
   apellido varchar(75)  NOT NULL,
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS cola (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla CLIENTE
+-- Estructura de tabla COLA
 --
 
 DROP TABLE IF EXISTS 'cola';
@@ -62,6 +62,40 @@ CREATE TABLE IF NOT EXISTS cola (
   nombre varchar(75)  NOT NULL,
   
   PRIMARY KEY (id_servicio)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla CENTRO
+--
+
+DROP TABLE IF EXISTS 'centro';
+
+CREATE TABLE IF NOT EXISTS centro (
+  id int NOT NULL AUTO_INCREMENT,
+  nombre varchar(75)  NOT NULL,
+  direccion varchar(155)  NOT NULL,
+  telefono varchar(9) NOT NULL,
+  ciudad varchar(75) NOT NULL,
+
+  PRIMARY KEY (id_servicio)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla AGENDA
+--
+
+DROP TABLE IF EXISTS 'agenda';
+
+CREATE TABLE IF NOT EXISTS agenda (
+  id int NOT NULL AUTO_INCREMENT,
+  hora time NOT NULL,
+  dia date NOT NULL,
+  
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
